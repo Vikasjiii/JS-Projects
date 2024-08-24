f252b83878051e8225c4ba14272668722ab3cc69
Vikas Sahni <vikassahni8361@gmail.com>
2024-08-24 14:49:49 +0530

    done

--- 02_Meme_Generator/script.js
+++ 02_Meme_Generator/script.js
@@ -2,27 +2,14 @@ const generateMemeButton = document.querySelector(".generate-meme-button");
 const memeTitle = document.querySelector(".meme-title");
 const memeImg = document.querySelector(".meme-img-container img");
 const memeAuthor = document.querySelector(".meme-author");
-const loadingText = document.querySelector(".loading");
-
 generateMemeButton.addEventListener("click", () => {
   fetch("https://meme-api.com/gimme")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      const { title, postLink, author } = data;
-
+      const { title, url, author } = data;
       memeAuthor.innerText = author;
       memeTitle.innerText = title;
-      
-console.log(postLink);
-
-      memeImg.src = postLink;
-      console.log(memeImg);
-
-      loadingText.style.display = "none";
+      memeImg.src = url;
     });
 });
-
-
-
 generateMemeButton.click();
