3c1d183d4f83509ac0f99e65da3f98d47d0f8fa0
Vikas Sahni <vikassahni8361@gmail.com>
2024-08-24 09:10:27 +0530

    completed

--- 01_ToDo/script.js
+++ 01_ToDo/script.js
@@ -4,53 +4,145 @@ const allToDoContainer = document.querySelector(".all-todos-container");
 const warningTextForEmptyTask = document.querySelector(
   ".warning-for-empty-task"
 );
+const statsContainer = document.querySelector(".stats");
+const totalTaskCountSpan = document.querySelector(".total-task-count");
+const completedTaskCountSpan = document.querySelector(".completed-task-count");
+const remainingTaskCountSpan = document.querySelector(".remaining-task-count");
+
 let taskCount = 0;
+let completedTaskCount = 0;
+const allSpanofSerailNumberforToDos = [];
 
 function addTaskInTheList() {
   const oneToDoContainer = document.createElement("div");
   oneToDoContainer.classList.add("one-todo-container");
   oneToDoContainer.innerHTML = `<span class="todo-number">${++taskCount})</span>
-        <input type="text" disabled="disabled"  value="${
+        <input type="text" spellcheck="false" disabled="disabled"  value="${
           userTaskInput.value
         }" />
+        
         <div class="controls">
-          <div class="complete-btn">complete</div>
-          <div class="edit-btn">edit</div>
-          <div class="delete-btn">delete</div>`;
+          <div title="complete" class="complete-btn"><img src="./check-icon.svg" alt=""></div>
+          <div title="edit" class="edit-btn"><img src="./edit-icon.png" alt="" style="width: 20px;"></div>
+          <div title="delete" class="delete-btn"><img src="./delete_img.svg" alt=""></div>
+          
+          `;
+  // <div title="setting" class="setting"><img src="./three-dots-icon.svg" alt=""></div>
 
-  allToDoContainer.appendChild(oneToDoContainer);
   userTaskInput.value = "";
+  statsContainer.classList.add("show");
+  totalTaskCountSpan.innerText = taskCount;
+  allToDoContainer.appendChild(oneToDoContainer);
+
+  //Adding span element of newly created onetodocontainer in the  allSpanofSerailNumberforToDos Array
+  allSpanofSerailNumberforToDos.push(
+    oneToDoContainer.querySelector(".todo-number")
+  );
+
   const deleteBtn = oneToDoContainer.querySelector(".delete-btn");
-  deleteBtn.addEventListener("click", () => {
-    deleteBtn.parentElement.parentElement.remove();
-    taskCount--;
-    updateToDoSerialNumber();
-  });
+  attachClickEventListenerToDeleteBtn(deleteBtn, oneToDoContainer);
 
   const completeBtn = oneToDoContainer.querySelector(".complete-btn");
-  completeBtn.addEventListener("click", () => {
-    completeBtn.parentElement.previousElementSibling.classList.toggle(
-      "cross-task-text"
-    );
-    console.log(completeBtn.parentElement.previousElementSibling);
 
-    completeBtn.classList.toggle("change-bg");
-  });
+  attachClickEventListenerToCompleteBtn(completeBtn, oneToDoContainer);
 
   const editBtn = oneToDoContainer.querySelector(".edit-btn");
-  editBtn.addEventListener("click", () => {
-    editBtn.parentElement.previousElementSibling.disabled = false;
-    console.log(editBtn.parentElement.previousElementSibling);
+  attachClickEventListenerToEditBtn(editBtn, oneToDoContainer, completeBtn);
 
-    editBtn.parentElement.previousElementSibling.focus();
+  // const settingIcon=oneToDoContainer.querySelector('.setting')
+  // attachClickEventListenerToSettingIcon(settingIcon,oneToDoContainer)
+}
+
+function attachClickEventListenerToCompleteBtn(
+  completeButton,
+  oneToDoContainerHTML
+) {
+  completeButton.addEventListener("click", () => {
+    const todoText = oneToDoContainerHTML.children[1];
+    todoText.classList.toggle("cross-task-text");
+    completeButton.classList.toggle("change-bg");
+
+    if (todoText.classList.contains("cross-task-text")) {
+      ++completedTaskCount;
+      completedTaskCountSpan.innerText = completedTaskCount;
+      remainingTaskCountSpan.innerText = taskCount - completedTaskCount;
+    } else {
+      --completedTaskCount;
+      completedTaskCountSpan.innerText = completedTaskCount;
+      remainingTaskCountSpan.innerText = taskCount - completedTaskCount;
+    }
   });
 }
 
-function updateToDoSerialNumber() {
-  const allSpanForSerailNumber = document.querySelectorAll(".todo-number");
+function attachClickEventListenerToEditBtn(
+  editButton,
+  oneToDoContainerHTML,
+  completeButton
+) {
+  editButton.addEventListener("click", () => {
+    editButton.classList.add("edit-bg");
+    const todoText = oneToDoContainerHTML.children[1];
+    if (todoText.classList.contains("cross-task-text")) {
+      --completedTaskCount;
+      completedTaskCountSpan.innerText = completedTaskCount;
+      remainingTaskCountSpan.innerText = taskCount - completedTaskCount;
+    }
+    todoText.classList.remove("cross-task-text");
+    completeButton.classList.remove("change-bg");
+    todoText.disabled = false;
+    todoText.focus();
 
-  allSpanForSerailNumber.forEach((spanForSerailNumber, i) => {
-    spanForSerailNumber.innerText = `${++i})`;
+    oneToDoContainerHTML.children[1].addEventListener("blur", () => {
+      todoText.disabled = true;
+      editButton.classList.remove("edit-bg");
+    });
+  });
+}
+function updateToDoSerialNumber(allSpanForToDoSerailNumber) {
+  allSpanForToDoSerailNumber.forEach((spanForToDoSerailNumber, i) => {
+    spanForToDoSerailNumber.innerText = `${i + 1})`;
+  });
+}
+
+function attachClickEventListenerToDeleteBtn(
+  deleteButton,
+  oneToDoContainerHTML
+) {
+  deleteButton.addEventListener("click", () => {
+    deleteButton.classList.add("red-bg");
+    const deleteTodoSerialNumberSpan = oneToDoContainerHTML.children[0];
+
+    --taskCount;
+    totalTaskCountSpan.innerText = taskCount;
+    //remove delete todo serail span
+    const deleteTodoSerialNumberSpanIndex =
+      allSpanofSerailNumberforToDos.findIndex(
+        (todoSerialNumberSpan) =>
+          todoSerialNumberSpan === deleteTodoSerialNumberSpan
+      );
+    allSpanofSerailNumberforToDos.splice(deleteTodoSerialNumberSpanIndex, 1);
+    console.log(oneToDoContainerHTML);
+
+    if (
+      oneToDoContainerHTML.children[1].classList.contains("cross-task-text")
+    ) {
+      --completedTaskCount;
+      completedTaskCountSpan.innerText = completedTaskCount;
+      remainingTaskCountSpan.innerText = taskCount - completedTaskCount;
+    }
+    setTimeout(() => {
+      oneToDoContainerHTML.remove();
+    }, 100);
+    updateToDoSerialNumber(allSpanofSerailNumberforToDos);
+  });
+}
+
+function attachClickEventListenerToSettingIcon(
+  settingIcon,
+  oneToDoContainerHTML
+) {
+  settingIcon.addEventListener("click", () => {
+    oneToDoContainerHTML.classList.add("show-setting");
   });
 }
 
@@ -63,11 +155,7 @@ addTaskButton.addEventListener("click", () => {
   }
 });
 
+//remove  warning Text For Empty Task when user focus on input
 userTaskInput.addEventListener("focus", () => {
   warningTextForEmptyTask.classList.remove("show");
 });
-
-for (let i = 0; i < 7; i++) {
-  userTaskInput.value = `${"vikassahni".slice(i)}`;
-  addTaskButton.click();
-}
