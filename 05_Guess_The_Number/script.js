57d3d4b7552123cbf9a479a8229ce5496980add7
Vikas Sahni <vikassahni8361@gmail.com>
2024-08-26 02:25:06 +0530

    completed

--- 05_Guess_The_Number/script.js
+++ 05_Guess_The_Number/script.js
@@ -4,13 +4,55 @@ const randomNumber = Math.ceil(Math.random() * 100);
 const hint = document.querySelector(".hint");
 const previoiusGuesses = document.querySelector(".previous-guess");
 const startGameButton = document.querySelector(".start-game-button");
+const submitButton = document.querySelector(".submit-btn");
 let userInputCount = 0;
+let startButtonClickable = false;
+
+function checkUserNumberInput(userNum) {
+  console.log("random num by computer", randomNumber);
+  previoiusGuesses.classList.remove("hide");
+  if (userNum === randomNumber) {
+    previoiusGuesses.innerText = `${previoiusGuesses.innerText} ${userNum},`; //
+    hint.innerText = `You got it! Congrats`; //
+    disableButtons();
+  } else if (userNum > randomNumber) {
+    hint.innerText = `Too high!`;
+    previoiusGuesses.innerText = `${previoiusGuesses.innerText} ${userNum},`;
+  } else {
+    hint.innerText = `Too low!`;
+    previoiusGuesses.innerText = `${previoiusGuesses.innerText} ${userNum},`;
+  }
+}
+function disableButtons() {
+  userNumberInput.disabled = true;
+  userNumberInput.classList.add("no-cursor");
+  submitButton.disabled = true;
+  submitButton.classList.add("no-cursor");
+  startGameButton.classList.remove("no-cursor");
+  startGameButton.classList.add("glow-start-btn");
+  startButtonClickable = true;
+}
+function restartGame() {
+  userInputCount = 0;
+  hint.innerText = "";
+  previoiusGuesses.classList.add("hide");
+  previoiusGuesses.innerText = `Your guesses:`;
+
+  userNumberInput.disabled = false;
+  userNumberInput.classList.remove("no-cursor");
+
+  submitButton.disabled = false;
+  submitButton.classList.remove("no-cursor");
+  startGameButton.classList.add("disable-btn");
+  startGameButton.classList.remove("glow-start-btn");
+  startButtonClickable = false;
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   //user guess can not be empty
   if (userNumberInput.value === "") {
+    alert("Enter a Number");
     return;
   }
   ++userInputCount;
@@ -19,51 +61,23 @@ form.addEventListener("submit", (e) => {
     userNumberInput.value = "";
     checkUserNumberInput(userGuessNumber);
   }
-
   if (userInputCount === 10) {
     hint.innerText = `You lost! The number was ${randomNumber}`;
-
-    userNumberInput.disabled = true;
-    userNumberInput.classList.add("disable-btn");
-    startGameButton.classList.remove("disable-btn");
-    startGameButton.classList.add("glow-start-btn");
+    disableButtons();
     return;
   }
 });
 
-function checkUserNumberInput(userNum) {
-  console.log("random num by computer", randomNumber);
-  previoiusGuesses.classList.remove("hide");
-
-  if (userNum === randomNumber) {
-    previoiusGuesses.innerText = `${previoiusGuesses.innerText} ${userNum},`;
-
-    hint.innerText = `You got it! Congrats`;
-    userNumberInput.disabled = true;
-    userNumberInput.classList.add("disable-btn");
-    startGameButton.classList.remove("disable-btn");
-    startGameButton.classList.add("glow-start-btn");
-  } else if (userNum > randomNumber) {
-    hint.innerText = `Too high!`;
-  } else {
-    hint.innerText = `Too low!`;
-    previoiusGuesses.innerText = `${previoiusGuesses.innerText} ${userNum},`;
+startGameButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (startButtonClickable) {
+    restartGame();
   }
-}
+});
 
-function restartGame() {
-  userInputCount = 0;
-  hint.innerText = "";
-  previoiusGuesses.classList.add("hide");
-  previoiusGuesses.innerText = `Your guesses:`;
-
-  userNumberInput.disabled = false;
-  userNumberInput.classList.remove("disable-btn");
-  startGameButton.classList.add("disable-btn");
-  startGameButton.classList.remove("glow-start-btn");
-}
 userNumberInput.focus();
 
-startGameButton.addEventListener("click", () => {
-  restartGame();
-});
+for (let i = 1; i < 10; i++) {
+  userNumberInput.value = i;
+  document.querySelector(".submit-btn").click();
+}
